0178ed3882c9bb2b4aa366fd8d6fbe10848f874c
ShiroYuuki27 <adityaekacool@gmail.com>
2023-05-13 05:10:57 +0700

    menambahkan Novel Detail Page

---
+++ app/src/main/java/com/sata/izonovel/NovelFavoriteActivity.java
@@ -0,0 +1,14 @@
+package com.sata.izonovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovelFavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novel_favorite);
+    }
+}
